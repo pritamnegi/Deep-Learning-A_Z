87b64a9b569f84f0a18dbe17078cfd37f35009a4
Pritam Negi <pritamdnegi@gmail.com>
2018-06-16 19:16:24 +0530

    Add code for making predictions on the test set by using ANN

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -63,10 +63,13 @@ classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = [
 # Fitting the ANN to the Training Data
 classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
+#Part 3 - MAking the predictions and evaluating the model
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
- 
+y_pred = (y_pred > 0.5) 
+
+
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
