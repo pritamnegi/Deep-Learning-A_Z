2ac767a1fa004d11e5d71b58f14a4290f058f91a
Pritam Negi <pritamdnegi@gmail.com>
2018-06-17 16:58:04 +0530

    Add code for homework challenge i.e. predicting the result for a single new observation

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -63,12 +63,32 @@ classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = [
 # Fitting the ANN to the Training Data
 classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
-#Part 3 - MAking the predictions and evaluating the model
+# Part 3 - Making the predictions and evaluating the model
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5) 
 
+# Predicting the result for a single new observation
+
+"""
+Predict if the customer with the following informations will leave the bank: 
+
+Geography: France
+Credit Score: 600
+Gender: Male
+Age: 40 years old
+Tenure: 3 years
+Balance: $60000
+Number of Products: 2
+Does this customer have a credit card ? Yes
+Is this customer an Active Member: Yes
+Estimated Salary: $50000
+
+"""
+
+new_prediction = classifier.predict(sc.transform(np.array([[0.0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])))
+new_prediction = (new_prediction > 0.5)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
