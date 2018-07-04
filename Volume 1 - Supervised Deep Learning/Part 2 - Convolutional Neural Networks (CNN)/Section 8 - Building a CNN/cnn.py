7b80c61d333ca8d5865fbb329e78957d3dbae0b7
Pritam Negi <pritamdnegi@gmail.com>
2018-07-04 17:29:44 +0530

    Add code for Max Pooling step

--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
@@ -5,7 +5,7 @@
 # Importing the Keras libraries and packages
 
 from keras.models import Sequential
-from keras.layers import Convolution2D
+from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
@@ -16,4 +16,9 @@ classifier = Sequential()
 
 # Step 1 - Convolution
 
-classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 3, 3), activation = 'relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+
+
+# Step 2 - Max Pooling
+
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
