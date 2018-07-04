57f907992ed6a2cba551087b536ef26e56369045
Pritam Negi <pritamdnegi@gmail.com>
2018-07-04 18:14:23 +0530

    Add code for Full Connection step

--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
@@ -11,19 +11,17 @@ from keras.layers import Flatten
 from keras.layers import Dense
 
 # Initialising the CNN
-
 classifier = Sequential()
 
 # Step 1 - Convolution
-
 classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
 
-
 # Step 2 - Max Pooling
-
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
-
 # Step 3 - Flattening
-
 classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(activation= 'relu', units = 128))
+classifier.add(Dense(activation= 'sigmoid', units = 1))
