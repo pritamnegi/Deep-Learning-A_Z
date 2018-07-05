6a991ec05f6ec3dbaf585879ebe861885c7b6828
Pritam Negi <pritamdnegi@gmail.com>
2018-07-05 17:39:44 +0530

    Added second convolution layer in the existing CNN to improve its performance

--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
@@ -19,6 +19,11 @@ classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'
 # Step 2 - Max Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+
 # Step 3 - Flattening
 classifier.add(Flatten())
 
