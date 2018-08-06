53e48e20c2ce3db01e1ac46a5b28197d99fc49b7
Pritam Negi <pritamdnegi@gmail.com>
2018-08-06 21:01:19 +0530

    Added code for initialising the RNN

--- Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
+++ Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
@@ -22,3 +22,14 @@ y_train = training_set[1:1258]
 
 # Reshaping
 X_train = np.reshape(X_train, (1257, 1, 1))
+
+
+# Part 2 - Building the RNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense 
+from keras.layers import LSTM
+
+# Initialising the RNN
+regressor = Sequential()
